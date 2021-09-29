e4eb68f1a9ed6ab696cea621e214b176191eda83
thaysom22 <thaysom22@gmail.com>
2021-09-29 16:23:15 +0000

    UPDATE VIEWS FOR ADDRESSES APP
    * refactor to override get_queryset instead of setting queryset attribute
    * edit docstrings for view classes and methods

--- addresses/views.py
+++ addresses/views.py
@@ -9,10 +9,9 @@ from .permissions import CurrentUserIsOwner
 
 class CurrentUserAddressListDestroyCreate(generics.GenericAPIView):
 	"""
-	View to create address or to list or delete all addresses for current user
+	View to create address or to list or delete all addresses for current user 
+    with optional request query parameter filtering
 	Methods allowed: GET, POST, DELETE
-	GET returns list of address instances associated with the current authenticated user
-	POST creates a new address instance associated with the current authenticated user
 	"""
 	search_fields = ['address1', 'address2', 'city', 'region', 'postcode', 'country'] 
 	filter_backends = [filters.SearchFilter]
@@ -20,11 +19,15 @@ class CurrentUserAddressListDestroyCreate(generics.GenericAPIView):
 	
 	def get_queryset(self):
         """
-        Return addresses for currently authenticated user only
+        Returns addresses for currently authenticated user only
         """
         return Address.objects.filter(user=self.request.user)
 	
 	def get(self, request):
+        """
+        Returns (optionally filtered) list of address instances associated with the 
+        current authenticated user 
+        """
 		user_addresses = self.get_queryset()
 		if user_addresses.exists():
 			serializer = self.get_serializer(user_addresses, many=True)
@@ -34,7 +37,10 @@ class CurrentUserAddressListDestroyCreate(generics.GenericAPIView):
 		return Response(status=status.HTTP_404_NOT_FOUND)
 		
 	def post(self, request):
-		address_data = request.data
+        """
+        Creates a new address instance associated with the current authenticated user
+		"""
+        address_data = request.data
 		# add the current user to payload for deserialization
 		address_data['user'] = request.user
 		serializer = self.get_serializer(data=address_data)
@@ -44,6 +50,10 @@ class CurrentUserAddressListDestroyCreate(generics.GenericAPIView):
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 	def delete(self, request):
+        """
+        Deletes all (or optionally filtered subset) address instances 
+        associated with the current authenticated user
+        """ 
 		user_addresses = self.get_queryset()
 		if user_addresses.exists():
 			serializer = self.get_serializer(user_addresses, many=True)
@@ -54,8 +64,16 @@ class CurrentUserAddressListDestroyCreate(generics.GenericAPIView):
 		return Response(status=status.HTTP_404_NOT_FOUND)
 
 
-# generic class provides get, put, patch and delete method handlers via built-in mixins
-class AddressRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView): 
+class AddressRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    """
+	View to retrieve, update or delete single address instances identified
+    by primary key
+	Methods allowed: GET, PUT, PATCH, DELETE
+    Generic class provides get, put, patch and delete method handler 
+    implementations via built-in mixins
+	""" 
 	queryset = Address.objects.all()
 	permissions_classes = [CurrentUserIsOwner|permissions.IsAdminUser]
-	serializer = AddressSerializer
+	serializer_class = AddressSerializer
+    search_fields = ['address1', 'address2', 'city', 'region', 'postcode', 'country'] 
+	filter_backends = [filters.SearchFilter]
